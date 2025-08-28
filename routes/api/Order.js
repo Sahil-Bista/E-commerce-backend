2a9e54e91035eb5508678cbdae33aec65d69f8eb
Sahil Bista <sahil@Sahils-MacBook-Air.local>
2025-08-28 12:46:50 +0545

    Feat: Added express validation to order routes

--- routes/api/Order.js
+++ routes/api/Order.js
@@ -2,10 +2,12 @@ import express from 'express';
 import { verifyJWT } from '../../middlewares/verifyJWT.js';
 import { verifyRoles } from '../../middlewares/verifyRoles.js';
 import { cancelOrder, createOrder, getAllOrders, getUserSpecificOngoingOrders, getUserSpecificOrderHistory } from '../../controllers/orderController.js';
+import {validate} from "../../middlewares/validate.js"; 
+import { cancelOrderValidator, createOrderValidation, getOrderValidtor } from '../../validators/orderValidator.js';
 export const OrderRouter  = express.Router();
 
-OrderRouter.get('/',verifyJWT, verifyRoles("Admin"), getAllOrders);
-OrderRouter.post('/:cartId',verifyJWT, verifyRoles('User'),createOrder)
+OrderRouter.get('/',verifyJWT, verifyRoles("Admin"), getOrderValidtor,validate , getAllOrders);
+OrderRouter.post('/:cartId',verifyJWT, verifyRoles('User'), createOrderValidation, validate, createOrder)
 OrderRouter.get('/trackOrder',verifyJWT, verifyRoles('User'),getUserSpecificOngoingOrders);
 OrderRouter.get('/orderHistory',verifyJWT, verifyRoles('User'),getUserSpecificOrderHistory);
-OrderRouter.patch('/:orderId',verifyJWT, verifyRoles('User'),cancelOrder)
+OrderRouter.delete('/:orderId',verifyJWT, verifyRoles('User'), cancelOrderValidator, validate, cancelOrder)
---
+++ validators/orderValidator.js
@@ -0,0 +1,31 @@
+import { param } from "express-validator";
+
+export const createOrderValidation = [
+    param('cartId')
+        .isEmpty()
+        .withMessage('Cart Id is required')
+        .isMongoId()
+        .withMessage('Cart Id must be a valid mongo object')
+]
+
+export const getOrderValidtor = [
+    query('page')
+        .optional()
+        .isInt({gt : 0})
+        .withMessage('Page number must be greater than 0')
+        .toInt(),
+    
+    query('limit')
+        .optional()
+        .isInt({git : 0})
+        .withMessage('Limit  must be greater than 0')
+        .toInt()
+]
+
+export const cancelOrderValidator = [
+    param('orderId')
+        .isEmpty()
+        .withMessage('Cart Id is required')
+        .isMongoId()
+        .withMessage('Cart Id must be a valid mongo object')
+]
